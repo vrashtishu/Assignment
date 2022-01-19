fbf4a545ac317daa141607792083410892075c9c
vrashtishu <65499182+vrashtishu@users.noreply.github.com>
2022-01-19 14:17:16 +0530

    Add Subtract Function

--- calculator.c
+++ calculator.c
@@ -3,8 +3,13 @@ int add(int a,int b)
 {
 	return a+b;
 }
+int sub(int a,int b)
+{
+	return a-b;
+}
 int main()
 {
 	int a=10,b=5;
 	printf("Addition of a & b is = %d",add(a,b));
+	printf("Subtraction of a & b is = %d",sub(a,b));
 }
