d3f46dc9d3a96678ac365baf0d1ddb6bb878f12a
jyotioct <98017149+jyotioct@users.noreply.github.com>
2022-01-19 14:31:23 +0530

    Add Division function for u

--- calculator.c
+++ calculator.c
@@ -11,10 +11,15 @@ int mul(int a,int b)
 {
 	return a*b;
 }
+int div(int a,int b)
+{
+	return a/b;
+}
 int main()
 {
 	int a=10,b=5;
 	printf("Addition of a & b is = %d",add(a,b));
 	printf("Subtraction of a & b is = %d",sub(a,b));
 	printf("Multipliaction of a & b is = %d",mul(a,b));
+	printf("Division of a & b is = %d",div(a,b));
 }
