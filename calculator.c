b12ff9d59f766ba1743cf58f92317a61904522c5
jyotioct <98017149+jyotioct@users.noreply.github.com>
2022-01-19 14:25:33 +0530

    Add Multiplication function

--- calculator.c
+++ calculator.c
@@ -7,9 +7,14 @@ int sub(int a,int b)
 {
 	return a-b;
 }
+int mul(int a,int b)
+{
+	return a*b;
+}
 int main()
 {
 	int a=10,b=5;
 	printf("Addition of a & b is = %d",add(a,b));
 	printf("Subtraction of a & b is = %d",sub(a,b));
+	printf("Multipliaction of a & b is = %d",mul(a,b));
 }
